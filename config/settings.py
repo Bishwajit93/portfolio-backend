82d049e70a0e90af55765ccbc5328c491579b3df
bishwajit93 <bish.karm123@gmail.com>
2025-06-13 00:50:41 +0200

    corshead changes

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 APPEND_SLASH = False
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
