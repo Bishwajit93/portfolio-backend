778af9b211fb633c77f8995cd4ea1c2054698e0e
bishwajit93 <bish.karm123@gmail.com>
2025-06-30 15:25:31 +0200

    Add experience, education, skills URLs

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from .views.projects import ProjectList, ProjectDetail
+from .views.experience import ExperienceList, ExperienceDetail
+from .views.education import EducationList, EducationDetail
+from .views.skill import SkillList, SkillDetail
+
 urlpatterns = [
+    # Projects
     path('projects/', ProjectList.as_view()),
     path('projects/<int:pk>/', ProjectDetail.as_view()),
+
+    # Experience
+    path('experience/', ExperienceList.as_view()),
+    path('experience/<int:pk>/', ExperienceDetail.as_view()),
+
+    # Education
+    path('education/', EducationList.as_view()),
+    path('education/<int:pk>/', EducationDetail.as_view()),
+
+    # Skills
+    path('skills/', SkillList.as_view()),
+    path('skills/<int:pk>/', SkillDetail.as_view()),
 ]
