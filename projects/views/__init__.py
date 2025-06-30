f3eabb35853560bd6e5bf1b908199337f0a82e80
bishwajit93 <bish.karm123@gmail.com>
2025-06-30 16:07:25 +0200

    Add experience, education, skill views to __all__

--- projects/views/__init__.py
+++ projects/views/__init__.py
@@ -1,7 +1,12 @@
 from .projects import ProjectList, ProjectDetail
+from .experience import ExperienceList, ExperienceDetail
+from .education import EducationList, EducationDetail
+from .skill import SkillList, SkillDetail
 
 
 __all__ = [
     "ProjectList", "ProjectDetail",
-
+    "ExperienceList", "ExperienceDetail",
+    "EducationList", "EducationDetail",
+    "SkillList", "SkillDetail",
 ]
