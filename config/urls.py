4d3464f92f51d831ecac08cba9ceb254f40e5efe
bishwajit93 <bish.karm123@gmail.com>
2025-07-27 01:54:34 +0200

    login page setup

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from projects.views.auth_custom import CustomLoginAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # JWT authentication endpoints
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    # Your app's API routes
     path("api/", include("projects.urls")),
-    
-    # Custom login page
-    path("auth/login/", CustomLoginAPIView.as_view(), name="custom-login")
 ]
